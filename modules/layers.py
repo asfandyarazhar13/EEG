52b7a2d0bf46c1e79135e4ab9a568e290b21eb80
Asfandyar Azhar <86783435+asfandyarazhar13@users.noreply.github.com>
2024-05-02 18:19:57 -0400

    Update layers.py

--- modules/layers.py
+++ modules/layers.py
@@ -245,3 +245,63 @@ class BiLSTMBlock(nn.Module):
         output, (hn, cn) = self.lstm(x)
         output = output[:, :, :self.lstm.hidden_size] + output[:, :, self.lstm.hidden_size:]  # Sum the outputs of the bidirectional LSTM
         return output[:, -1, :]  # Return the last sequence element
+
+class WaveBlock(nn.Module):
+    """
+    A building block for the WaveNet architecture utilizing dilated convolutions.
+
+    Attributes:
+        num_rates (int): Number of dilation levels.
+        convs (nn.ModuleList): List of convolutional layers.
+        filter_convs (nn.ModuleList): List of dilated convolutional layers for filter.
+        gate_convs (nn.ModuleList): List of dilated convolutional layers for gate.
+    """
+    def __init__(self, in_channels: int, out_channels: int, dilation_rates: int, kernel_size: int = 3):
+        """
+        Initializes the Wave_Block module.
+
+        Args:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+            dilation_rates (int): Number of dilation levels.
+            kernel_size (int): Size of the kernel for convolution operations.
+        """
+        super(WaveBlock, self).__init__()
+        self.num_rates = dilation_rates
+        self.convs = nn.ModuleList([nn.Conv1d(in_channels, out_channels, kernel_size=1, bias=True)])
+        dilation_rates = [2 ** i for i in range(dilation_rates)]
+
+        for dilation_rate in dilation_rates:
+            self.filter_convs.append(
+                nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size,
+                          padding=int((dilation_rate * (kernel_size - 1)) / 2), dilation=dilation_rate))
+            self.gate_convs.append(
+                nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size,
+                          padding=int((dilation_rate * (kernel_size - 1)) / 2), dilation=dilation_rate))
+            self.convs.append(nn.Conv1d(out_channels, out_channels, kernel_size=1, bias=True))
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        """Initializes weights of the convolution layers using Xavier uniform distribution."""
+        for conv in self.convs:
+            nn.init.xavier_uniform_(conv.weight, gain=nn.init.calculate_gain('relu'))
+            nn.init.zeros_(conv.bias)
+
+        for filter_conv, gate_conv in zip(self.filter_convs, self.gate_convs):
+            nn.init.xavier_uniform_(filter_conv.weight, gain=nn.init.calculate_gain('relu'))
+            nn.init.zeros_(filter_conv.bias)
+            nn.init.xavier_uniform_(gate_conv.weight, gain=nn.init.calculate_gain('relu'))
+            nn.init.zeros_(gate_conv.bias)
+
+    def forward(self, x):
+        """Forward pass of the Wave_Block."""
+        x = self.convs[0](x)
+        res = x
+        for i in range(self.num_rates):
+            tanh_out = torch.tanh(self.filter_convs[i](x))
+            sigmoid_out = torch.sigmoid(self.gate_convs[i](x))
+            x = tanh_out * sigmoid_out
+            x = self.convs[i + 1](x)
+            res += x
+        return res
